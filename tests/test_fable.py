5f619340d2c23cdfc7ca739f576988df51ea5d13
Yannis Katsaros <yannis@katsaros.io>
2020-09-24 10:58:11 -0400

    Add test suite for float parser

--- tests/test_fable.py
+++ tests/test_fable.py
@@ -1,7 +1,10 @@
+from math import isnan, isinf
+
 from fable.fable import (
     Version,
     Parser,
     Integer,
+    Float,
     Error,
     ErrorCode,
 )
@@ -54,3 +57,137 @@ class TestIntegerParser:
         s = 'integer my_int "1995"'
         result = Parser.parse_integer(s)
         assert result.code == ErrorCode.PARSING_ERROR
+
+class TestFloatParser:
+    def test_generic_float(self):
+        s = 'float num 4'
+        result = Parser.parse_float(s)
+        assert result == Float('num', 4.0, False)
+
+    def test_all_nans(self):
+        nans = [
+            'float num nan',
+            'float num NaN',
+            'float num NaN%',
+            'float num NAN',
+            'float num NaNQ',
+            'float num NaNS',
+            'float num qNaN',
+            'float num sNaN',
+            'float num 1.#SNAN',
+            'float num 1.#QNAN',
+            'float num +nan.0'
+        ]
+        for nan in nans:
+            result = Parser.parse_float(nan)
+            assert isnan(result.value)
+
+    def test_all_infs(self):
+        infs = [
+            'float num inf',
+            'float num +inf',
+            'float num -inf'
+        ]
+        for inf in infs:
+            result = Parser.parse_float(inf)
+            assert isinf(result.value)
+
+    def test_positive_signed_floats(self):
+        cases = [
+            'float num +14.51',
+            'float num +1.451E01',
+            'float num +1.451E+01',
+            'float num +1.451e01',
+            'float num +1.451e+01'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', 14.51, False)
+
+        cases = [
+            'float num +0.001451',
+            'float num +1.451E-03',
+            'float num +1.451e-03'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', 0.001451, False)
+
+    def test_nullable_positive_signed_floats(self):
+        cases = [
+            'float? num +14.51',
+            'float? num +1.451E01',
+            'float? num +1.451E+01',
+            'float? num +1.451e01',
+            'float? num +1.451e+01',
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', 14.51, True)
+
+        cases = [
+            'float? num +0.001451',
+            'float? num +1.451E-03',
+            'float? num +1.451e-03'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', 0.001451, True)
+
+    def test_negative_signed_floats(self):
+        cases = [
+            'float num -14.51',
+            'float num -1.451E01',
+            'float num -1.451E+01',
+            'float num -1.451e01',
+            'float num -1.451e+01'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', -14.51, False)
+
+        cases = [
+            'float num -0.001451',
+            'float num -1.451E-03',
+            'float num -1.451e-03'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', -0.001451, False)
+
+    def test_nullable_negative_signed_floats(self):
+        cases = [
+            'float? num -14.51',
+            'float? num -1.451E01',
+            'float? num -1.451E+01',
+            'float? num -1.451e01',
+            'float? num -1.451e+01'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', -14.51, True)
+
+        cases = [
+            'float? num -0.001451',
+            'float? num -1.451E-03',
+            'float? num -1.451e-03'
+        ]
+        for s in cases:
+            result = Parser.parse_float(s)
+            assert result == Float('num', -0.001451, True)
+
+    def test_separator_notation_1(self):
+        s = 'float num 281_979'
+        result = Parser.parse_float(s)
+        assert result == Float('num', 281979, False)
+
+    def test_separator_notation_2(self):
+        s = 'float num 281_979.441_512'
+        result = Parser.parse_float(s)
+        assert result == Float('num', 281979.441512, False)
+
+    def test_separator_notation_3(self):
+        # weird, but technically possible
+        s = 'float num 28_19_79.44_15_12'
+        result = Parser.parse_float(s)
+        assert result == Float('num', 281979.441512, False)
